9b011540fff416a5d6945033d8b5098913353a1a
MorganeFar <131769763+MorganeFar@users.noreply.github.com>
2024-11-05 16:57:50 +0100

    feat : deep in open cv

--- data.py
+++ data.py
@@ -1,12 +1,13 @@
 import pandas as pd
 from data_functions import *
 import time
-
+import cv2
+import numpy as np
 
 # x = longitude (vertical)
 # y = latitude (horizontal)
 start_time = time.perf_counter()
-path = "data/bathymetry_small_area_japan_sea.csv"
+path = "data/bathymetry_zoom_in_japan_sea.csv"
 
 # Read csv file into a dataframe
 dataFrame = pd.read_csv(path)
@@ -16,7 +17,23 @@ if isinstance(dataFrame['latitude'][0], str):
     dataFrame = dataFrame.drop([0])
 
 matrixH = createMatrix(dataFrame)
-print(matrixH)
+array = np.zeros([len(matrixH), len(matrixH[0]), 3], dtype=np.uint8)
+
+deepMax = findDeepMax(matrixH)
+print(deepMax)
+
+for i in range(len(matrixH)) :
+    for j in range(len(matrixH[i])):
+        # Not water
+        if matrixH[i][j]>=0.0 :
+            array[i,j]=[0,255,0]
+        else:
+            array[i,j] = [(abs(deepMax)-abs(matrixH[i][j]))%255, 0, 0]
+        
+cv2.imwrite('bat_img.jpg', array)
+cv2.imshow("image", array)
+cv2.waitKey()
+
 
 end_time = time.perf_counter()
 execution_time = end_time - start_time
--- data_functions.py
+++ data_functions.py
@@ -1,9 +1,31 @@
+import numpy as np
+
 def deletePositiveZ(dataFrame):
     for index, row in dataFrame.iterrows():
         if row['z'] > 0.0:
             dataFrame = dataFrame.drop(index)
     return dataFrame
 
+
+def createMatrix2(dataFrame):
+    matrix = np.array([])
+    sizeDataSet = len(dataFrame)
+    index = 1
+
+    while index < sizeDataSet:
+        height = dataFrame['z'][index]
+        # Add the first height data to the matrix
+        row = np.array([height])
+        current_lat = dataFrame['latitude'][index]
+        studied_lat = current_lat
+
+        while studied_lat == current_lat and index < sizeDataSet:
+            index += 1
+            height = dataFrame['z'][index]
+            row=np.append(row, height)
+            studied_lat = dataFrame['latitude'][index]
+        matrix=np.append(matrix, row)
+    return matrix
 
 def createMatrix(dataFrame):
     matrix = []
@@ -13,15 +35,23 @@ def createMatrix(dataFrame):
     while index < sizeDataSet:
         height = dataFrame['z'][index]
         # Add the first height data to the matrix
-        matrix.append([height])
+        row = [height]
         current_lat = dataFrame['latitude'][index]
         studied_lat = current_lat
-
         while studied_lat == current_lat and index < sizeDataSet:
             index += 1
             height = dataFrame['z'][index]
-            matrix[len(matrix)-1].append(height)
+            row.append(height)
             studied_lat = dataFrame['latitude'][index]
+        matrix.append(row)
 
     return matrix
 
+
+def findDeepMax(matrix):
+    mini = min(matrix[0])
+    for i in range(len(matrix)):
+        if mini > min(matrix[i]):
+            mini = min(matrix[i])
+    return mini
+            
