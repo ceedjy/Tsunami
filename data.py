f838c7c6e1b65e2cb0fa474d2d89841f39b28cd0
MorganeFar <131769763+MorganeFar@users.noreply.github.com>
2024-11-07 11:31:52 +0100

    feat : calcul speed + line between 2 points

--- data.py
+++ data.py
@@ -1,34 +1,100 @@
 import pandas as pd
 from data_functions import *
-import time
 import cv2
 import numpy as np
+from tsunami import * 
+from math import fabs
 
+NB_CLICS = 2
+TAB_CLICS = []
 
 # function to display the coordinates of 
 # of the points clicked on the image  
 def click_event(event, x, y, flags, params): 
-  
+    global NB_CLICS
+    global TAB_CLICS
+    
     # Left mouse clicks 
-    if event == cv2.EVENT_LBUTTONDOWN: 
-        # displaying X on image
-        sizeX = cv2.getTextSize('X', 0, 1.0, 1)
-        # displaying the coordinates 
-        # on the image window 
-        font = cv2.FONT_HERSHEY_SIMPLEX 
-        cv2.putText(array, 'X', (x-(sizeX[0][0]//2),y+(sizeX[0][1]//2)), font, 
-                    1, (0, 0, 255), 2) 
-        cv2.imshow('image', array) 
+    if NB_CLICS > 0:
+        if event == cv2.EVENT_LBUTTONDOWN: 
+            # displaying X on image
+            sizeX = cv2.getTextSize('X', 0, 1.0, 1)
+            # displaying the coordinates 
+            # on the image window 
+            font = cv2.FONT_HERSHEY_SIMPLEX 
+            cv2.putText(array, 'X', (x-(sizeX[0][0]//2),y+(sizeX[0][1]//2)), font, 
+                        1, (0, 0, 255), 2) 
+            cv2.imshow('image', array)
+            TAB_CLICS.append((x,y))
+            NB_CLICS -= 1
+            
+            if NB_CLICS == 0 :
+                cv2.line(array, TAB_CLICS[0], TAB_CLICS[1], (0,0,255), 1)
+                TAB_CLICS = []
+                NB_CLICS = 2
+
   
   
+def bresenham_march(img, p1, p2):
+     x1 = p1[0]
+     y1 = p1[1]
+     x2 = p2[0]
+     y2 = p2[1]
+     #tests if any coordinate is outside the image
+     if ( 
+         x1 >= img.shape[0]
+         or x2 >= img.shape[0]
+         or y1 >= img.shape[1]
+         or y2 >= img.shape[1]
+     ): #tests if line is in image, necessary because some part of the line must be inside, it respects the case that the two points are outside
+         if not cv2.clipLine((0, 0, *img.shape), p1, p2):
+             print("not in region")
+             return
+
+     steep = fabs(y2 - y1) > fabs(x2 - x1)
+     if steep:
+         x1, y1 = y1, x1
+         x2, y2 = y2, x2
+
+     # takes left to right
+     also_steep = x1 > x2
+     if also_steep:
+         x1, x2 = x2, x1
+         y1, y2 = y2, y1
+
+     dx = x2 - x1
+     # fabs return the absolute value of a number (abs() = absolute value but transform to float)
+     dy = fabs(y2 - y1)
+     error = 0.0
+     delta_error = 0.0
+     # Default if dx is zero
+     if dx != 0:
+         delta_error = fabs(dy / dx)
+
+     y_step = 1 if y1 < y2 else -1
+
+     y = y1
+     ret = []
+     for x in range(x1, x2):
+         p = (y, x) if steep else (x, y)
+         if p[0] < img.shape[0] and p[1] < img.shape[1]:
+             ret.append(p)
+         error += delta_error
+         if error >= 0.5:
+             y += y_step
+             error -= 1
+     if also_steep:  # because we took the left to right instead
+         ret.reverse()
+     return ret  
+  
+    
+def restart_window():
+    pass
     
   
 # driver function 
 if __name__=="__main__": 
   
-    # x = longitude (vertical)
-    # y = latitude (horizontal)
-    start_time = time.perf_counter()
     path = "data/bathymetry_zoom_in_japan_sea.csv"
 
     # Read csv file into a dataframe
@@ -50,22 +116,32 @@ if __name__=="__main__":
                 array[i,j]=[0,255,0]
             else:
                 array[i,j] = [(abs(deepMax)-abs(matrixH[i][j]))%255, 0, 0]
+    
+    # Copy do not touch
+    base_map = array.copy()
+    nb_clics = 2
+    
 
+    points_line = bresenham_march(array, (0,0), (125, 50))
+    speed_sum = 0
+    for point in points_line :
+        speed_sum += speed(g, abs(matrixH[point[0]][point[1]]))
+    speed_final = speed_sum / len(points_line)
+    print(f'Sum : {speed_final} m/s')
+    print(f'Time : {distance((0,0), (125, 50))/speed_final} s')
+    
     cv2.imwrite('bat_img.jpg', array)
-    cv2.imshow("image", array)          
-    end_time = time.perf_counter()
-    execution_time = end_time - start_time
+    cv2.imshow("image", array) 
+    
+    if nb_clics > 0:
+        cv2.setMouseCallback('image', click_event) 
+        nb_clics -= 1
 
-    print(f"Programme exécuté en : {execution_time: .5f} secondes")
-  
-    # setting mouse handler for the image 
-    # and calling the click_event() function 
-    cv2.setMouseCallback('image', click_event) 
-  
     # wait for a key to be pressed to exit 
     cv2.waitKey(0) 
-  
+    
     # close the window 
-    cv2.destroyAllWindows() 
+    cv2.destroyAllWindows()
+    
 
 
